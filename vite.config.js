f743d6057833d72dd3c5c48ef0b7fb7bc508dfe0
Patbob123 <flyingbikeddd@gmail.com>
2023-09-01 20:33:37 -0400

    tialwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { chromeExtension } from 'vite-plugin-chrome-extension'
+import { ViteComponents } from 'vite-plugin-components'
+
+import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: 'src/manifest.json',
+    },
+  },
+  plugins: [
+    react(), 
+    ViteComponents ({
+      extensions: ['react'],
+      customComponentResolvers: [
+        ViteIconsResolver({
+          componentPrefix: '',
+        }),
+      ],
+    }),
+    ViteIcons(),
+    chromeExtension()],
 })
