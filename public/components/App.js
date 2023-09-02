b3a54aa6d0bfbbe34eca75af7425311f14cce37e
Patbob123 <flyingbikeddd@gmail.com>
2023-09-02 01:20:57 -0400

    popupcss

--- public/components/App.js
+++ public/components/App.js
@@ -1,27 +1,25 @@
 import { getStorage, removeStorage } from "../modules/storage.js";
 import * as time from '../modules/time.js';
 function App() {
-  let [visitCount, setVisitCount] = React.useState(0);
-  React.useEffect(() => {
-    let abc = async () => {
-      let x = await getStorage("www.youtube.com");
-      let y = x["www.youtube.com"];
-      console.log(y);
-      setVisitCount(y);
-    };
-    abc();
-  }, []);
   return /*#__PURE__*/React.createElement("div", {
-    style: {
-      height: '20rem',
-      width: '20rem'
-    }
-  }, /*#__PURE__*/React.createElement("div", null, visitCount?.start, " ", visitCount?.current, " ", time.compareTime(visitCount.current, visitCount.start)), /*#__PURE__*/React.createElement("div", {
+    className: "flex flex-col w-80 h-80 bg-slate-900"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "flex w-full h-1/3 space-x-4 px-5 py-4"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "flex-none items-center w-16 h-16"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "./assets/icon128.png",
+    alt: "Icon",
+    className: ""
+  })), /*#__PURE__*/React.createElement("div", {
+    className: "flex flex-1 w-auto h-16 items-center justify-center"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "font-extrabold text-3xl text-transparent text-center bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-600 duration-700"
+  }, "UNTANGLED"))), /*#__PURE__*/React.createElement("div", {
+    className: "flex w-full h-1/3 px-5 py-4 justify-center bg-red-600"
+  }, /*#__PURE__*/React.createElement("div", {
     onClick: async () => await removeStorage("www.youtube.com"),
-    style: {
-      backgroundColor: 'red',
-      textAlign: 'center'
-    }
-  }, "RESET"));
+    className: "h-1/3 rounded font-extrabold bg-yellow-50"
+  }, "RESET")));
 }
 ReactDOM.createRoot(document.querySelector('#root')).render( /*#__PURE__*/React.createElement(App, null));
--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,26 @@ import { getStorage, removeStorage } from "../modules/storage.js";
 import * as time from '../modules/time.js'
 
 function App() {
-    let [visitCount, setVisitCount] = React.useState(0);
-
-    React.useEffect(() => {
-        let abc = async () => {
-            let x = await getStorage("www.youtube.com")
-            let y = x["www.youtube.com"]
-            console.log(y)
-            setVisitCount(y)
-        }
-        abc()
-
-    },[])
+    
     return (
-        <div style={{height:'20rem',width:'20rem'}}>
-        <div>
-            {visitCount?.start} {visitCount?.current} {time.compareTime(visitCount.current, visitCount.start)}
-        </div>
-            <div onClick={async () => await removeStorage("www.youtube.com")} style={{ backgroundColor: 'red', textAlign: 'center' }}>
-                RESET
+            <div className="flex flex-col w-80 h-80 bg-slate-900">
+                <div className="flex w-full h-1/3 space-x-4 px-5 py-4">
+                    <div className="flex-none items-center w-16 h-16">
+                        <img src="./assets/icon128.png" alt="Icon" className=""></img>
+                    </div>
+                    <div className="flex flex-1 w-auto h-16 items-center justify-center">
+                        <div className="font-extrabold text-3xl text-transparent text-center bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-600 duration-700">
+                            UNTANGLED
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className="flex w-full h-1/3 px-5 py-4 justify-center bg-red-600">
+                    <div onClick={async () => await removeStorage("www.youtube.com")} className="h-1/3 rounded font-extrabold bg-yellow-50">
+                        RESET
+                    </div>
+                </div>
             </div>
-        </div>
     );
 }
 
