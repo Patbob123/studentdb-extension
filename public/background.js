e37a0a2567a2e5e521886a04c9f9ff0eea702d12
348670050 <haloday873@gmail.com>
2023-09-01 22:01:29 -0400

    filter

--- public/background.js
+++ public/background.js
@@ -1,7 +1,17 @@
 chrome.tabs.onActivated.addListener(
-    function(activeInfo) {
-      chrome.tabs.get(activeInfo.tabId, function(tab) {
-        console.log(activeInfo.tabId)
-      });
+    function (activeInfo) {
+        chrome.tabs.get(activeInfo.tabId, function (tab) {
+            // console.log(activeInfo.tabId)
+        });
     }
-  );
+);
+const filter = {
+    url: [
+        // { urlMatches: 'https://www.google.com/' },
+        { urlMatches: 'https://www.youtube.com/' }
+    ],
+};
+
+chrome.webNavigation.onCompleted.addListener(() => {
+    console.info("The user has loaded my favorite website!");
+}, filter);
